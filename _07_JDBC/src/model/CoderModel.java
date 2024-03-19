7b6b03b2104d1866da19e85e2fe97e2d40cc2b38
Kevin Mejia <kwmejia9@gmail.com>
2024-03-19 11:32:51 -0500

    JDBC

--- _07_JDBC/src/model/CoderModel.java
+++ _07_JDBC/src/model/CoderModel.java
@@ -1,7 +1,15 @@
 package model;
 
 import database.CRUD;
+import database.ConfigDB;
+import entity.Coder;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoderModel implements CRUD {
@@ -13,7 +21,46 @@ public class CoderModel implements CRUD {
 
     @Override
     public List<Object> findAll() {
-        return null;
+        // 1. Crear lista para guardar lo que nos devuelve la base de datos
+        List<Object> listCoders = new ArrayList<>();
+
+        // 2. Abrir la conexión
+        Connection objConnection = ConfigDB.openConnection();
+
+        try {
+            // 3. Escribimos el query en Sql
+            String sql = "SELECT * FROM coder;";
+
+            //4. Usar el prepareStatement
+            PreparedStatement objPrepare = objConnection.prepareStatement(sql);
+
+            //5. Ejecutar el query y obtener el resultado (ResulSet)
+
+            ResultSet objResult = objPrepare.executeQuery();
+
+            // 6. Mientras haya un resultado siguiente hacer:
+            while (objResult.next()){
+
+                // 6.1 Crear un coder
+                Coder objCoder = new Coder();
+
+                //6.2 Llenar el objeto con la información de la base de datos del objeto ques está iterando
+                objCoder.setName(objResult.getString("name"));
+                objCoder.setAge(objResult.getInt("age"));
+                objCoder.setId(objResult.getInt("id"));
+                objCoder.setClan(objResult.getString("clan"));
+
+                //6.3 Agregamos el Coder a la lista
+                listCoders.add(objCoder);
+            }
+
+        }catch (SQLException error){
+            JOptionPane.showMessageDialog(null, error.getMessage());
+        }
+        //Paso 7. Cerrar la conexión
+        ConfigDB.closeConnection();
+
+        return listCoders;
     }
 
     @Override
